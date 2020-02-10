79911ebb37ff19a9da84a46fae08ec30c7d1c02e
Delvric <mr.tezeno@gmail.com>
2020-02-10 11:43:48 -0600

    string

--- string1.py
+++ string1.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
-"""
-Kenzie assignment: String1
-"""
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
-
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
-
-# Instructions:
-# Complete each function below by writing the code for it. main() is already
-# set up to test all the functions with a few different inputs, printing 'OK' when
-# each function returns the correct result.
-# The starter code for each function includes a bare 'return' which is just a
-# placeholder for your code.
-
+# Basic string exercises
+# Fill in the code for the functions below. main() is already set up
+# to call the functions with a few different inputs,
+# printing 'OK' when each function is correct.
+# The starter code for each function includes a 'return'
+# which is just a placeholder for your code.
+# It's ok if you do not complete all the functions, and there
+# are some additional functions to try in string2.py.
 # A. donuts
 # Given an int count of a number of donuts, return a string
 # of the form 'Number of donuts: <count>', where <count> is the number
@@ -26,20 +22,23 @@ Kenzie assignment: String1
 
 
 def donuts(count):
-    # your code here
-    return
-
+    if count < 10:
+        return "Number of donuts: " + str(count)
+    else:
+        return "Number of donuts: many"
 
 # B. both_ends
 # Given a string s, return a string made of the first 2
 # and the last 2 chars of the original string,
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
+
+
 def both_ends(s):
-    # your code here
-    return
-
-
+    if len(s) < 2:
+        return ''
+    return s[:2] + s[-2:]
+# for loop to run threw string// first 2 skip 2
 # C. fix_start
 # Given a string s, return a string
 # where all occurences of its first char have
@@ -49,11 +48,11 @@ def both_ends(s):
 # Assume that the string is length 1 or more.
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
+
+
 def fix_start(s):
-    # your code here
-    return
-
-
+    s = s[0] + s[1:].replace(s[0], '*')
+    return s
 # D. MixUp
 # Given strings a and b, return a single string with a and b separated
 # by a space '<a> <b>', except swap the first 2 chars of each string.
@@ -61,46 +60,49 @@ def fix_start(s):
 #   'mix', pod' -> 'pox mid'
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
+
+
 def mix_up(a, b):
-    # your code here
-    return
-
-
+    new_a = b[:2] + a[2:]
+    new_b = a[:2] + b[2:]
+    return new_a + ' ' + new_b
 # Provided simple test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
+
+
 def test(got, expected):
+    """Your code goes here.  Edit this docstring."""
     if got == expected:
         prefix = ' OK '
     else:
         prefix = '  X '
-    print('{} got: {}    expected: {}'.format(prefix, repr(got), repr(expected)))
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
 
-
-# The main() func calls the above functions with interesting inputs,
+# Provided main() calls the above functions with interesting inputs,
 # using test() to check if each result is correct or not.
+
+
 def main():
+    """Your code goes here.  Edit this docstring."""
     print('donuts')
-    # Each line calls donuts, and compares its result to the expected return for that call.
+    # Each line calls donuts, compares its result to the expected for that call.
     test(donuts(4), 'Number of donuts: 4')
     test(donuts(9), 'Number of donuts: 9')
     test(donuts(10), 'Number of donuts: many')
     test(donuts(99), 'Number of donuts: many')
-
-    print('')
+    print()
     print('both_ends')
     test(both_ends('spring'), 'spng')
     test(both_ends('Hello'), 'Helo')
     test(both_ends('a'), '')
     test(both_ends('xyz'), 'xyyz')
-
-    print('')
+    print()
     print('fix_start')
     test(fix_start('babble'), 'ba**le')
     test(fix_start('aardvark'), 'a*rdv*rk')
     test(fix_start('google'), 'goo*le')
     test(fix_start('donut'), 'donut')
-
-    print('')
+    print()
     print('mix_up')
     test(mix_up('mix', 'pod'), 'pox mid')
     test(mix_up('dog', 'dinner'), 'dig donner')
@@ -109,6 +111,5 @@ def main():
 
 
 # Standard boilerplate (python idiom) to call the main() function.
-# This is called an "import guard"
 if __name__ == '__main__':
     main()
--- string2.py
+++ string2.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
-"""
-Kenzie Assignment: String2
-"""
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
-
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
-
-# Instructions:
-# Complete each of these string exercises in the same way as the
-# previous String1 excercises.
-
+# Additional basic string exercises
 # D. verbing
 # Given a string, if its length is at least 3,
 # add 'ing' to its end.
@@ -23,10 +15,13 @@ Kenzie Assignment: String2
 
 
 def verbing(s):
-    # your code here
-    return
-
-
+    length = len(s)
+    if length > 2:
+        if s[-3:] == 'ing':
+            s += 'ly'
+        else:
+            s += 'ing'
+    return s
 # E. not_bad
 # Given a string, find the first appearance of the
 # substring 'not' and 'bad'. If the 'bad' follows
@@ -35,11 +30,14 @@ def verbing(s):
 # Return the resulting string.
 # So 'This dinner is not that bad!' yields:
 # This dinner is good!
+
+
 def not_bad(s):
-    # your code here
-    return
-
-
+    q = s.split("bad")
+    w = q[0].split("not")
+    if len(q) > 1 < len(w):
+        return w[0] + "good" + "bad".join(q[1:])
+    return s
 # F. front_back
 # Consider dividing a string into two halves.
 # If the length is even, the front and back halves are the same length.
@@ -47,43 +45,53 @@ def not_bad(s):
 # e.g. 'abcde', the front half is 'abc', the back half 'de'.
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
+
+
 def front_back(a, b):
-    # your code here
-    return
-
-
+    def splitter(s):
+        l = len(s)
+        if l % 2 == 0:
+            d = l/2
+        else:
+            d = (l/2) + 1
+        return (s[:d], s[d:])
+    a1, a2 = splitter(a)
+    b1, b2 = splitter(b)
+    return a1 + b1 + a2 + b2
 # Provided simple test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
+
+
 def test(got, expected):
+    """Your code goes here.  Edit this docstring."""
     if got == expected:
         prefix = ' OK '
     else:
         prefix = '  X '
-    print('{} got: {}     expected: {}'.format(prefix, repr(got), repr(expected)))
-
-
+    print('{} got: {} expected: {}'.format(prefix, repr(got), repr(expected)))
 # main() calls the above functions with interesting inputs,
 # using the above test() to check if the result is correct or not.
+
+
 def main():
+    """Your code goes here.  Edit this docstring."""
     print('verbing')
     test(verbing('hail'), 'hailing')
     test(verbing('swiming'), 'swimingly')
     test(verbing('do'), 'do')
-
-    print('')
+    print()
     print('not_bad')
     test(not_bad('This movie is not so bad'), 'This movie is good')
     test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
     test(not_bad('This tea is not hot'), 'This tea is not hot')
     test(not_bad("It's bad yet not"), "It's bad yet not")
-
-    print('')
+    print()
     print('front_back')
     test(front_back('abcd', 'xy'), 'abxcdy')
     test(front_back('abcde', 'xyz'), 'abcxydez')
     test(front_back('Kitten', 'Donut'), 'KitDontenut')
 
 
-# This is called the 'import guard' -- it calls the main function.
+# Standard boilerplate (python idiom) to call the main() function.
 if __name__ == '__main__':
     main()
